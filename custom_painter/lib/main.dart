f3498a7c1d15b349cbc02f8fe82e1c439f24262b
ivansaul <saul_2503@hotmail.com>
2023-06-22 14:06:29 -0500

    add custom_painter

--- custom_painter/lib/main.dart
+++ custom_painter/lib/main.dart
@@ -36,7 +36,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
-      home: const HomeScreen(),
+      home: const IntroScreen(),
       debugShowCheckedModeBanner: false,
       theme: ThemeData(useMaterial3: true),
     );
